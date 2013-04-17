b99cd7167577713547190edc05748e6578196ca9
Marc Boßerhoff <kontakt@marcbosserhoff.de>
2013-04-17 21:03:37 +0200

    A little more CRUD

--- war/src/main/java/de/marcbosserhoff/ui/ExampleForm.java
+++ war/src/main/java/de/marcbosserhoff/ui/ExampleForm.java
@@ -1,7 +1,84 @@
 package de.marcbosserhoff.ui;
 
-import com.vaadin.ui.VerticalLayout;
+import com.github.wolfie.blackboard.Blackboard;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import de.marcbosserhoff.service.ExampleService;
+import de.marcbosserhoff.ui.event.ReloadEntriesEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-public class ExampleForm extends VerticalLayout {
+@Component
+@Scope("prototype")
+public class ExampleForm extends FormLayout {
 
+    private Logger log = LoggerFactory.getLogger(ExampleForm.class);
+
+    private Blackboard blackboard;
+
+    @Autowired
+    private ExampleService exampleService;
+
+    private TextField name = new TextField("Name:");
+
+    private TextField address = new TextField("Address:");
+
+    public ExampleForm() {
+        initForm();
+    }
+
+    private void initForm() {
+        addComponent(name);
+        addComponent(address);
+
+        final Button commit = new Button("Commit");
+        final Button cancel = new Button("Cancel");
+
+        cancel.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                clearAndHide();
+            }
+        });
+        commit.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                commitForm();
+                fireCommitEvent();
+                clearAndHide();
+            }
+        });
+
+        final HorizontalLayout buttonBar = new HorizontalLayout();
+
+        buttonBar.addComponent(commit);
+        buttonBar.addComponent(cancel);
+
+        addComponent(buttonBar);
+    }
+
+    private void commitForm() {
+        log.info("Creating user with name {} and address {}", name.getValue(), address.getValue());
+        exampleService.createUserWithNameAndAdress(name.getValue(), address.getValue());
+    }
+
+    private void clearAndHide() {
+        name.setValue("");
+        address.setValue("");
+        setVisible(false);
+    }
+
+    private void fireCommitEvent() {
+        log.info("Fire commit event!");
+        blackboard.fire(new ReloadEntriesEvent());
+    }
+
+    public void setBlackBoard(Blackboard blackboard) {
+        this.blackboard = blackboard;
+    }
 }
--- war/src/main/java/de/marcbosserhoff/ui/MyVaadinUI.java
+++ war/src/main/java/de/marcbosserhoff/ui/MyVaadinUI.java
@@ -1,12 +1,15 @@
 package de.marcbosserhoff.ui;
 
+import com.github.wolfie.blackboard.Blackboard;
 import com.vaadin.annotations.Title;
+import com.vaadin.event.ItemClickEvent;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 import de.marcbosserhoff.model.ExampleEntity;
 import de.marcbosserhoff.repositories.ExampleRepository;
+import de.marcbosserhoff.ui.event.ReloadEntriesEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -20,7 +23,11 @@ import java.util.List;
 @Component
 @Scope("prototype")
 @Title("Example Vaadin Application")
-public class MyVaadinUI extends UI {
+public class MyVaadinUI extends UI implements ReloadEntriesEvent.ReloadEntriesListener {
+
+    private Logger log = LoggerFactory.getLogger(MyVaadinUI.class);
+
+    private Blackboard blackboard = new Blackboard();
 
     @Autowired
     private ExampleContainer exampleContainer;
@@ -28,34 +35,109 @@ public class MyVaadinUI extends UI {
     @Autowired
     private ExampleRepository exampleRepository;
 
+    private Button addButton;
+    private Button deleteButton;
+    private Table entityTable;
+    private Long selectedId;
+
+    @Autowired
+    private ExampleForm editForm;
+
     @Override
     protected void init(VaadinRequest request) {
         initData();
         initLayout();
+        registerEvents();
     }
 
     private void initData() {
         List<ExampleEntity> all = exampleRepository.findAll();
+        exampleContainer.removeAllItems();
         exampleContainer.addAll(all);
     }
 
     private void initLayout() {
         final VerticalLayout layout = new VerticalLayout();
+        layout.setWidth("400px");
 
-        final Table entityTable = new Table();
+        entityTable = new Table();
         entityTable.setContainerDataSource(exampleContainer);
-
         entityTable.setVisibleColumns(ExampleContainer.PROPERTIES);
         entityTable.setColumnHeaders(ExampleContainer.HEADERS);
+        entityTable.setSelectable(true);
+        entityTable.setWidth("100%");
+        entityTable.setHeight("300px");
 
-        final ExampleForm editForm = new ExampleForm();
+        entityTable.addItemClickListener(new ItemClickEvent.ItemClickListener() {
+            @Override
+            public void itemClick(ItemClickEvent event) {
+                selectedId = (Long) event.getItemId();
+
+                log.info("Selected item id {}", selectedId);
+            }
+        });
+
+        final AbstractLayout buttonBar = initButtonBar();
+        buttonBar.setWidth("100%");
+
+        editForm.setBlackBoard(blackboard);
+        editForm.setVisible(false);
 
         layout.addComponent(entityTable);
+        layout.addComponent(buttonBar);
         layout.addComponent(editForm);
 
         layout.setMargin(true);
         setContent(layout);
     }
 
+    private AbstractLayout initButtonBar() {
+        final HorizontalLayout buttonBar = new HorizontalLayout();
 
+        buttonBar.setSpacing(true);
+
+        addButton = new Button("Add entry");
+        deleteButton = new Button("Delete entry");
+
+        buttonBar.addComponent(addButton);
+        buttonBar.addComponent(deleteButton);
+
+        buttonBar.setComponentAlignment(addButton, Alignment.MIDDLE_LEFT);
+        buttonBar.setComponentAlignment(deleteButton, Alignment.MIDDLE_RIGHT);
+
+        addButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                editForm.setVisible(true);
+            }
+        });
+        deleteButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                removeSelectedEntry();
+            }
+        });
+
+        return buttonBar;
+    }
+
+    private void removeSelectedEntry() {
+        if (selectedId != null) {
+            exampleRepository.delete(selectedId);
+            blackboard.fire(new ReloadEntriesEvent());
+        }
+    }
+
+    private void registerEvents() {
+        blackboard.enableLogging();
+        blackboard.register(ReloadEntriesEvent.ReloadEntriesListener.class, ReloadEntriesEvent.class);
+        blackboard.addListener(this);
+    }
+
+    @Override
+    public void reloadEntries(ReloadEntriesEvent event) {
+        log.info("Received reload event. Refreshing entry table!");
+        initData();
+        entityTable.markAsDirty();
+    }
 }
