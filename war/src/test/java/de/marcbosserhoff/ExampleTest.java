4c37b895d2df9cb81b8ec3b75e24420c63259718
Marc Boßerhoff <kontakt@marcbosserhoff.de>
2013-04-23 21:33:50 +0200

    Added unit test examples, but entities are not working right now

---
+++ war/src/test/java/de/marcbosserhoff/ExampleTest.java
@@ -0,0 +1,16 @@
+package de.marcbosserhoff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/testContext.xml" } )
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+
+    }
+}
